da50236dc2ed6ea032f67d18c57787751efa54c2
rishikasrinivas <118067369+rishikasrinivas@users.noreply.github.com>
2025-12-10 12:13:31 -0800

    Update test_questions.py

--- DUNE_polaris_cursor/benchmarking/test_questions.py
+++ DUNE_polaris_cursor/benchmarking/test_questions.py
@@ -1,17 +1,13 @@
-from src.api.argo_client import ArgoAPIClient
-from config import (
-    HOST, PORT, DEBUG, ARGO_API_USERNAME, ARGO_API_KEY, 
-    DEFAULT_TOP_K, ENABLE_AUTHENTICATION, FERMILAB_REDIRECT_URI, 
-    validate_config, create_directories
-)
+from src.api.fermi_client import FermilabAPIClient
 from src.indexing.chroma_manager import ChromaManager
 from collections import defaultdict
-argo = ArgoAPIClient(ARGO_API_USERNAME, ARGO_API_KEY)
-
+import pandas as pd
 import os
 
+fermi = FermilabAPIClient()
 chroma_manager = ChromaManager(os.getenv('DB_PATH'))
 results=defaultdict(list)
+
 questions = [
     'How many CPA planes will be installed in the DUNE Horizontal Drift Far Detector in South Dakota',
     'Can you find a subset of talks from Laura Fields Neutrino Beam experiments?',
@@ -23,14 +19,15 @@ questions = [
     'Can you summarize the updates to DUNE’s science goals presented at the recent Fermilab Colloquium by Chris Marshall?',
     'When did the 2×2 demonstrator start collecting data, and how long did data taking last?',
     'Can you provide a list of current DUNE Speakers Committee members?',
-        ]
+]
+
 for question in questions:
-    context_snippet, ref = chroma_manager.search(question,top_k=3)
+    context_snippet, ref = chroma_manager.search(question)
     context = "\n\n".join(context_snippet)
 
-    answer = argo.chat_completion(question, context)
+    answer = fermi.chat_completion(question, context)
     results['Question'].append(question)
     results['Answer'].append(answer)
     results['Links'].append(' '.join(ref))
-import pandas as pd
-pd.DataFrame(results).to_csv("TestQuestions_nodistinciton_search.csv")
+
+pd.DataFrame(results).to_csv("Results/TestQuestions_results.csv")
