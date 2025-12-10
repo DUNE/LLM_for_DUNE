f6e705234112908b4ca20ce34a6ce48985e570ff
rishikasrinivas <118067369+rishikasrinivas@users.noreply.github.com>
2025-12-10 12:08:25 -0800

    Update evaluation.py

--- DUNE_polaris_cursor/benchmarking/evaluation.py
+++ DUNE_polaris_cursor/benchmarking/evaluation.py
@@ -4,12 +4,12 @@ from mlflow.genai import scorer
 from mlflow.genai.scorers import Correctness, Guidelines
 import json
 from config import ( ARGO_API_USERNAME, ARGO_API_KEY, 
-    DEFAULT_TOP_K, STORE, QA_PATH
+    DEFAULT_TOP_K, STORE, QA_PATH, FERMILAB_SESSION_SECRET
 )
+import re
 import time
 import logging
 import pandas as pd
-from config import FERMILAB_SESSION_SECRET
 from src.indexing.chroma_manager import ChromaManager
 from src.api.fermilab_client import FermilabAPIClient
 from src.api.argo_client import ArgoAPIClient
@@ -26,104 +26,121 @@ from src.extractors.indico_extractor_multithreaded import IndicoExtractor
 from src.extractors.docdb_extractor_multithreaded import DocDBExtractor
 
 
-fermi_client=FermilabAPIClient(ARGO_API_USERNAME, ARGO_API_KEY)
-MODEL='gpt-oss:20b'
+
 
 indico_session=IndicoExtractor()
 docdb_session=DocDBExtractor()
 docdb_session._build_session()
 
-
-def normalize(s):
-    # Remove all whitespace characters like \n, \t, spaces, etc.
-
-    expected = re.sub(r'\s+', '', s)
-    if expected[-1] == '.': 
-        return expected[:-1]
-    return expected
-
-def relevant_refs(question, expected, contexts, references):
-    '''
-        expected: str (ground truth link)
-        contexts: list[str] (context snippets)
-        references: list[str] (link context snippets came from)
-        question: str (question)
-
-        Check if the references found the expected link and if so return a 1
-        If not pass all context snippets to an LLM (MODEL) to decide if it's a good set of snippets to use and give an associated score between 0 and 1
-    '''
-    expect = normalize(expected)
+class Utils():
+    def extract_https_url(self, text):
+        match = re.search(r'(?:\(|\[|\{)?(https?://[^\s\)\]\}]+)(?:\)|\]|\})?', text)
+        if match:
+            return match.group(1)  # Group 1 contains just the URL
+        return None
     
-    if expect in references:
-        return 1
-    else:
-        prompt = f'Read this context and determine if it provides an answer to the question: {question}, if so, return a float value closer to 1 in the format of a json with the only permitted key being "score". If the content in the file is not related to the question at all or loosely relates to the question, give a score close to 0'
-        results = 0
+    def normalize(self, s):
+        # Remove all whitespace characters like \n, \t, spaces, etc.
+    
+        expected = re.sub(r'\s+', '', s)
+        if expected[-1] == '.': 
+            return expected[:-1]
+        return expected
         
-            
-        resp=fermi_client.chat_completion(question=prompt, context=' '.join(contexts),  model=MODEL) #base_url='https://vllm.fnal.gov/v1/chat/completions')
-        match = re.search(r'(\d[\d\s]*\.?[\d\s]*)', resp)
-
-        if match:
-                # Remove spaces to clean up
-            number_str = match.group(1).replace(" ", "")
-
-        results += float(number_str)
-    return results
-
-def correctness(main_question, expectation, output) -> float:
-    '''
-        expectation: str (ground truth response)
-        output: str (DUNE GPT's response)
-        main_question: str (question)
-
-        Check if the answer is factually correct and answers the question using the ground truth as a guide (score continuous on [0,1])
-    '''
-    results = 0
-
-    try:
-        if '[ERROR]' in output:
-            print(output)
-            return 0.0
-        question=f"Determine if the generated output is correct. Return a float value between 0 and 1 in the format of a json with the only permitted key being 'score'. Your float value must not contain any letters, they must strictly be comprised of numerical values and decimals. You will evaluate correctness like this:  If the generated response is factually correct and provides a well formed answer to the question {main_question} return a score closer to 1. If the response is not well formed or not factually correct return a score closer to 0. Use the expected response as a guide to evaluate the generated response. Do not treat the expected response as the ground truth. Even if the generated response does not meet the guidelines of the expected response as long as the generated response is factually correct return a high score."
-                            
-        context = f"Generated output: {output}\nExpected out: {expectation}"
-        resp = fermi_client.chat_completion(question=question, context=context, model =MODEL) #'nomic-embed-text:latest')# base_url='https://vllm.fnal.gov/v1/chat/completions')
-        match = re.search(r'(\d[\d\s]*\.?[\d\s]*)', resp)
-
-        if match:
-            # Remove spaces to clean up
-            number_str = match.group(1).replace(" ", "")
+class Metrics():
+    def __init__(self, eval_model, api_client):
+        
+        self.client=FermilabAPIClient() if not api_client else api_client
+        self.model = 'gpt-oss:20b' if eval_model else eval_model
+        self.util_client = Utils()
+        
+        
+    def relevant_refs(self, question, expected, contexts, references):
+        '''
+            expected: str (ground truth link)
+            contexts: list[str] (context snippets)
+            references: list[str] (link context snippets came from)
+            question: str (question)
+    
+            Check if the references found the expected link and if so return a 1
+            If not pass all context snippets to an LLM (MODEL) to decide if it's a good set of snippets to use and give an associated score between 0 and 1
+        '''
+        expect = self.util_client.normalize(expected)
+        
+        if expect in references:
+            return 1
         else:
-            number_str = 0.0
-        results += float(number_str)
-    except Exception as e:
-        print("Excpetion ", e)
-        return e
-    return results
-
-
-import re
-
-def extract_https_url(text):
-    match = re.search(r'(?:\(|\[|\{)?(https?://[^\s\)\]\}]+)(?:\)|\]|\})?', text)
-    if match:
-        return match.group(1)  # Group 1 contains just the URL
-    return None
+            prompt = f'Read this context and determine if it provides an answer to the question: {question}, if so, return a float value closer to 1 in the format of a json with the only permitted key being "score". If the content in the file is not related to the question at all or loosely relates to the question, give a score close to 0'
+            results = 0
+            
+                
+            resp=self.client.chat_completion(question=prompt, context=' '.join(contexts),  model=self.model)
+            match = re.search(r'(\d[\d\s]*\.?[\d\s]*)', resp)
+    
+            if match:
+                    # Remove spaces to clean up
+                number_str = match.group(1).replace(" ", "")
+    
+            results += float(number_str)
+        return results
+    
+    def correctness(self, main_question, expectation, output) -> float:
+        '''
+            expectation: str (ground truth response)
+            output: str (DUNE GPT's response)
+            main_question: str (question)
+    
+            Check if the answer is factually correct and answers the question using the ground truth as a guide (score continuous on [0,1])
+        '''
+        results = 0
+    
+        try:
+            if '[ERROR]' in output:
+                print("Error: ", output)
+                return 0.0
+            question=f"Determine if the generated output is correct. Return a float value between 0 and 1 in the format of a json with the only permitted key being 'score'. Your float value must not contain any letters, they must strictly be comprised of numerical values and decimals. You will evaluate correctness like this:  If the generated response is factually correct and provides a well formed answer to the question {main_question} return a score closer to 1. If the response is not well formed or not factually correct return a score closer to 0. Use the expected response as a guide to evaluate the generated response. Do not treat the expected response as the ground truth. Even if the generated response does not meet the guidelines of the expected response as long as the generated response is factually correct return a high score."
+                                
+            context = f"Generated output: {output}\nExpected out: {expectation}"
+            resp = self.client.chat_completion(question=question, context=context, model =self.model) 
+            match = re.search(r'(\d[\d\s]*\.?[\d\s]*)', resp)
+    
+            if match:
+                # Remove spaces to clean up
+                number_str = match.group(1).replace(" ", "")
+            else:
+                number_str = 0.0
+            results += float(number_str)
+        except Exception as e:
+            print("Exception ", e)
+            return e
+        return results
+    
 
 class Evalutation():
     def __init__(self, port, experiment_name, data_path,model, top_k, keyword):
         self.chroma_manager = ChromaManager(data_path)
   
         self.model=model
-        self.fermi_client=FermilabAPIClient(ARGO_API_USERNAME, ARGO_API_KEY)
+        self.fermi_client=FermilabAPIClient()
      
         self.top_K=top_k
         self.keyword=keyword
 
-        
+        self.util_client= Utils()
+        self.metrics = Metrics()
+
+           
     def create_validation_dataset(self):
-        
+        '''
+        Creates a 'correctness' dataset where each entry is:
+
+        {
+            'inputs': 
+                {'question': "some question here"}, 
+            'expectations': 
+                {'expected_response': 'expected answer'}
+        }
+        '''
         qa=pd.read_csv(QA_PATH)
         qas=[]
         for row in qa.iterrows():
@@ -137,7 +154,16 @@ class Evalutation():
         self.eval_dataset = qas
     
     def create_refs_dataset(self):
-    
+        '''
+        Creates a 'relevant refs' dataset where each entry is:
+
+        {
+            'inputs': 
+                {'question': "some question here"}, 
+            'expectations': 
+                {'expected_response': 'www.link.com/pathtowebsite'}
+        }
+        '''
         refs_dataset=pd.read_csv(QA_PATH)
         refs=[]
         for row in refs_dataset.iterrows():
@@ -145,7 +171,7 @@ class Evalutation():
             dictionary['inputs'] = {'question': row[1]['question']}
             try:
                 link = re.sub(r'\s+', '', row[1]['link'])
-                link=extract_https_url(link)
+                link=self.util_client.extract_https_url(link)
                 assert link, row[1]['link']
             
                 dictionary['expectations'] = {'expected_response': link}
@@ -153,22 +179,13 @@ class Evalutation():
             except:
                 continue
         self.ref_dataset = refs
-    def create_latency_dataset(self):
-        latency_dataset=pd.read_csv(QA_PATH)
-        latency=[]
-        for row in latency_dataset.iterrows():
-            dictionary={}
-            dictionary['inputs'] = {'question': row[1]['question']}
-            dictionary['expectations'] = {'expected_response': 0}
-            latency.append(dictionary)
-        print("made latenvy ds")
-        self.latency_dataset = latency
-    
+ 
     def evaluate(self,method):
         self.create_validation_dataset()
 
         print(f"Starting eval: {method}")
-        
+
+        # Evaluates how 'correct' DUNEGPT's response is
         if method == 'correctness':
             self.create_validation_dataset()
             score = 0
@@ -178,12 +195,12 @@ class Evalutation():
                 expected = dictionary['expectations']['expected_response']
                 contexts, references = self.chroma_manager.search(question, top_k = self.top_K)
                 answer = self.fermi_client.chat_completion(question, " ".join(contexts), model=self.model)
-                score_temp = correctness(question, expected, answer)
+                score_temp = self.metrics.correctness(question, expected, answer)
                 df.append({'question': question, 'expected_response': expected, 'score': score_temp, 'true response': answer, 'contexts': contexts})
                 score += score_temp
             return df, [{'score' : score/len(self.eval_dataset)}]
 
-
+        # Evaluates how correct the retrieved chunks are
         elif method == 'relevant_refs':
             self.create_refs_dataset()
             score = 0
@@ -193,26 +210,29 @@ class Evalutation():
                 reference = dictionary['expectations']['expected_response']
                 
                 contexts, references = self.chroma_manager.search(question)
-                score_temp = relevant_refs(question, reference, contexts, references)
+                score_temp = self.metrics.relevant_refs(question, reference, contexts, references)
                 df.append({'question': question, 'expected_reference': reference, 'score': score_temp, 'true references': references, 'contexts': contexts})
                 score += score_temp
 
             return df, [{'score' : score/len(self.ref_dataset)}]
-
+            
+        # Evaluates how long it takes end-to-end
         elif method=='latency':
-            duration=0.0
-            qa=pd.read_csv(QA_PATH)
+            duration = 0.0
+            qa = pd.read_csv(QA_PATH)
             df = []
             for q in qa.iterrows():
                 question = q[1]['question']
+                
                 start=time.time()
                 context,links=self.chroma_manager.search(question, top_k=self.top_K, keyword=self.keyword)
                 resp=self.fermi_client.chat_completion(question, ' '.join(context), model=self.model)
                 end=time.time()
+                
                 df.append({'question': q, 'latency' : end-start})
                 duration += (end-start)
            
-            return df, [{'latency/mean': duration/100}]
+            return df, [{'score': duration/len(df}]
 
 import argparse
 
@@ -250,6 +270,7 @@ logger = init_logger('benchmarking_logger')
 val = Evalutation(args.port, args.experiment_name, args.data_path, args.model, args.top_k, args.keyword)
 results, score = val.evaluate(args.method)
 
+# Log the overall score and the per sample feedback/score 
 if isinstance(results, list):
     pd.DataFrame(results).to_csv(os.path.join(args.savedir, f'{args.method}_tracking.csv'))
     pd.DataFrame(score).to_csv(os.path.join(args.savedir, f'{args.method}_score.csv'))
